6735e209a61f9364cda7cfe48f6d44aedcbfe384
dattp6501 <truongphucdat6501@gmail.com>
2024-06-21 23:44:34 +0700

    update

--- src/main/java/com/dattp/order/service/KafkaService.java
+++ src/main/java/com/dattp/order/service/KafkaService.java
@@ -5,28 +5,27 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 public class KafkaService {
-    @Autowired @Lazy private KafkaTemplate<String, BookingResponseDTO> kafkaTemplateBooking;
+    @Autowired
+    @Lazy
+    private KafkaTemplate<String, BookingResponseDTO> kafkaTemplateBooking;
 
-    @Async("taskExecutor")
-    public boolean send(String topic, Object data){
+    public boolean send(String topic, Object data) {
         try {
-            if(data instanceof BookingResponseDTO){
+            if (data instanceof BookingResponseDTO) {
                 kafkaTemplateBooking.send(topic, (BookingResponseDTO) data);
-            }
-            else{
+            } else {
                 log.debug("======> KafkaService::send::{}::{} NOT CONFIG SEND KAFKA!!!", topic, data);
                 return false;
             }
             log.debug("======> KafkaService::send::{}::{} SUCCESS!!!", topic, data);
             return true;
-        }catch (Exception e){
-            log.debug("======> KafkaService::send::{}::{}::Exception:{}",topic, data, e.getMessage());
+        } catch (Exception e) {
+            log.debug("======> KafkaService::send::{}::{}::Exception:{}", topic, data, e.getMessage());
             return false;
         }
     }
