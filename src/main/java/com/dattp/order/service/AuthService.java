683e1b1f8537e8cca9d87b7ebd80559b2de15c86
dattp6501 <truongphucdat6501@gmail.com>
2025-02-06 13:03:53 +0700

    log

--- src/main/java/com/dattp/order/service/AuthService.java
+++ src/main/java/com/dattp/order/service/AuthService.java
@@ -1,15 +1,16 @@
 package com.dattp.order.service;
 
-import com.dattp.order.dto.auth.AuthResponseDTO;
-import com.dattp.order.exception.BadRequestException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
+@Log4j2
 public class AuthService extends com.dattp.order.service.Service {
   public Map<String, Object> verify(String accessToken) {
     Map<String, Object> detail = jwtService.getDetail(accessToken);
+    log.debug("=====================> verify: detail = \n{}", detail);
 
 //    AuthResponseDTO tokenOld = tokenStorage.get((Long) detail.get("id"));
 //    if (!tokenOld.getAccessToken().equals(accessToken)) throw new BadRequestException("Token invalid");
